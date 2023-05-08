f01da99469f712669f86c3ef67e705cf05f3e83e
Xavier Guerrero <xavierignacioguerrero@gmail.com>
2023-05-08 00:43:12 -0300

    0.0 primeros params

---
+++ src/constantes/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colores: {
+    primario: '#00a697',
+    oscuro: '#007267',
+    claro: '#00f2dc',
+    text: '#212121',
+    white: '#ffffff',
+    black: '#000000',
+    grey: '#cccccc',
+  },
+};
